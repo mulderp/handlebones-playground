e08cf177a818fbd6387899279a316dc1e308fed3
Patrick Mulder <mulder.patrick@gmail.com>
2014-05-03 11:57:45 +0200

    use handlebones

--- app/main.js
+++ app/main.js
@@ -3,20 +3,23 @@ var $ = require('jquery-untouched');
 Backbone.$ = $;
 
 var Handlebars = require('hbsfy/runtime');
+var Handlebones = require('handlebones/handlebones');
 
 Handlebars.registerHelper("view", function (view) {
-    if (!view) {
-      return "";
-    }
-    var htmlAttributes = {
-      // ensure generated placeholder tag in template
-      // will match tag of view instance
-      tagName: view.el.tagName.toLowerCase()
-    };
-    htmlAttributes[viewPlaceholderAttributeName] = view.cid;
-    var output = "stub"; // Handlebones.Util.tag(htmlAttributes, "", this);
-    return new Handlebars.SafeString(output);
+  if (!view) {
+    return "";
+  }
+  var htmlAttributes = {
+    // ensure generated placeholder tag in template
+    // will match tag of view instance
+    tagName: view.el.tagName.toLowerCase()
+  };
+  htmlAttributes[viewPlaceholderAttributeName] = view.cid;
+  var output = Handlebones.Util.tag(htmlAttributes, "", this);
+  return new Handlebars.SafeString(output);
 });
+
+debugger
 
 var HelloView = require('views/hello');
 
